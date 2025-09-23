16827f303245059375147a6ed5756ecea6f329c8
Hannah Hailu Muthoni <muthonihannahhailu@gmail.com>
2025-09-23 08:25:24 +0000

    Sending link to the backend and link validation completed

--- server.js
+++ server.js
@@ -5,7 +5,25 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));  // for form data
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, "public")));
+
+app.post('/link', (req, res) => {
+  const link = req.body.link;
+  
+  // Validation of the link
+  const regex = /^https:\/\/www\.youtube\.com\/watch\?v=/;
+  const match = regex.test(link); // true
+  
+  if (!match) {
+    res.send('Invalid link. Try again!')
+    return
+  }
+
+  res.send('Hello');
+})
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
